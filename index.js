5ec9cd8f2f8974ffd7edf70e8db9b21689a62d7f
Paul <paulnyoiken@gmail.com>
2022-11-22 10:09:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+  if (ride <= 400) {
+    return ('This one is on me!')
+  } else if (ride > 2000 && ride < 2500) {
+    return ('I will gladly take your thirty bucks.')
+  } else if (ride > 2500) {
+    return ('No can do.')
+  }
+ }
+ scuberGreetingForFeet()
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
 }
+ternaryCheckCity()
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return ("Thank you so much.")
+      break;
+      case 'not as generous':
+        return ("Thank you.")
+        break;
+    default:
+      return("Bye.")
+      break;
+  }
+}
+switchOnCharmFromTip()
